f25289b1a7962949b15765953656e67f804612ca
Jose Luis Lopez Arevalo <123702001+Luar44@users.noreply.github.com>
2024-02-14 08:31:52 -0600

    Poor desing and code for window

--- xd.py
+++ xd.py
@@ -1,19 +1,22 @@
-from PyQt6.QtWidgets import QApplication, QWidget, QLabel
+import pandas as pd
+import PyQt6.QtWidgets as xd 
 
-# Crea una instancia de la aplicación
-app = QApplication([])
+app = xd.QApplication([])
 
-# Crea una ventana
-window = QWidget()
-window.setWindowTitle("Ventana demasiado Cute")
-window.setGeometry(100, 100, 300, 200)  # Establece las dimensiones de la ventana
+window = xd.QWidget()
+window.setWindowTitle("XDXXXXEDDDDDxdddx")
+window.setGeometry(100, 100, 1000, 900)
 
-# Crea una etiqueta con el mensaje
-label = QLabel("Holi, soy muy cute 7w7 uwu", parent=window)
-label.setGeometry(50, 50, 200, 30)  # Posición y tamaño de la etiqueta
 
-# Muestra la ventana
+layout = xd.QVBoxLayout()
+
+xdbutton = xd.QPushButton("XDddXDdddxd")
+layout.addWidget(xdbutton)
+
+window.setLayout(layout) 
+
 window.show()
 
-# Inicia el bucle de eventos
 app.exec()
+
+#https://github.com/Luar44/Activity-04
